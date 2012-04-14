7aa04fd770008845f3e5397f9a38a795b54205ab
Stephen Bronstein <steve@bronstein.org>
2012-04-14 15:41:00 -0700

    finished admin section of tutorial

--- klingsbo/settings.py
+++ klingsbo/settings.py
@@ -1,3 +1,11 @@
+import os
+import django
+
+# calculated paths for django and the site
+# used as starting points for various other paths
+DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
 # Django settings for klingsbo project.
 
 DEBUG = True
@@ -106,10 +114,9 @@ ROOT_URLCONF = 'klingsbo.urls'
 WSGI_APPLICATION = 'klingsbo.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+  os.path.join(SITE_ROOT, 'templates')
 )
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,24 @@
 from polls.models import Poll
+from polls.models import Choice
+
 from django.contrib import admin
 
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3
+  
 class PollAdmin(admin.ModelAdmin):
-  fields = ['pub_date', 'question']
-
+  list_display = ('question', 'pub_date', 'was_published_recently')
+  list_filter = ['pub_date']
+  search_fields =['question']
+  date_hierarchy = 'pub_date'
+  
+  fieldsets = [
+    (None,                {'fields': ['question']}),
+    ('Date information',  {'fields' : ['pub_date'], 'classes': ['collapse']}),
+  ]
+  inlines = [ChoiceInline]
+  
 admin.site.register(Poll, PollAdmin)
+
+

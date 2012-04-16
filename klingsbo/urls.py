449ae6513ea664e848b06d8a156b6979d52216f2
Stephen Bronstein <steve@bronstein.org>
2012-04-15 22:14:59 -0700

    added url match for root

--- klingsbo/urls.py
+++ klingsbo/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
-
+from django.http import HttpResponseRedirect
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^polls/',include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', lambda x: HttpResponseRedirect('/polls/')),
 )

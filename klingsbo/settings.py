507366be956a12d2e9185cf66c05ad86168303e4
Stephen Bronstein <steve@bronstein.org>
2012-06-01 18:07:45 -0700

    update for new heroku db env methodology

--- klingsbo/settings.py
+++ klingsbo/settings.py
@@ -30,17 +30,11 @@ try:
       }
   }
 except:  
-  # example dev config with postgres
-  DATABASES = {
-      'default': {
-          'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-          'NAME': 'klingsbo',                      # Or path to database file if using sqlite3.
-          'USER': 'klingsbo',                      # Not used with sqlite3.
-          'PASSWORD': 'klingsbo',                  # Not used with sqlite3.
-          'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-          'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-      }
-  }
+  
+  # the new Heroku way
+  import dj_database_url
+  DATABASES = {'default': dj_database_url.config(default='postgres://klingsbo:klingsbo@localhost/klingsbo')}
+
   
   # example dev config w sqlite
   #DATABASES = {
